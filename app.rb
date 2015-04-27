3b500fc14839911876584f98f3e81b3f5c984a64
Jesse James & Drew <student@epicodus.com>
2015-04-27 11:11:03 -0700

    integration test to verify form submission and results

--- app.rb
+++ app.rb
@@ -3,3 +3,7 @@ require('sinatra/reloader')
 require('./lib/triangle')
 also_reload('lib/**/*.rb')
 require('pry')
+
+get('/') do
+  erb(:index)
+end
--- spec/triangle_integration_spec.rb
+++ spec/triangle_integration_spec.rb
@@ -6,7 +6,17 @@ set(:show_exceptions, false)
 describe('Wordcount Integration Testing', {:type => :feature}) do
   #framework test
   it('verifies a test route is getting to a test layout page') do
-    visit('/index')
+    visit('/')
     expect(page).to have_content('Triangle Type')
   end
+
+  it('verifies test route from form to results page') do
+    visit('/')
+    fill_in('side_a', :with => 1)
+    fill_in('side_b', :with => 1)
+    fill_in('side_c', :with => 1)
+    click_button('Send')
+    expect(page).to have_content('equilateral')
+  end
+
 end
